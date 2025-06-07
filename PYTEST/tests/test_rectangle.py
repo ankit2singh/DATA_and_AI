b7e0261aac31f04b2dc54055ede57ad30179f02d
Ankit Kumar Singh <107424618+ankit2singh@users.noreply.github.com>
2025-06-07 18:28:49 +0000

    Fixture 1

--- PYTEST/tests/test_rectangle.py
+++ PYTEST/tests/test_rectangle.py
@@ -1,10 +1,26 @@
 from Sources import shapes
 
-def test_area():
-    result = shapes.rectangle(2, 8)
-    assert result.area() == 2 * 8
+import pytest
+
+@pytest.fixture
+
+def my_rectangle():
+    return shapes.rectangle(10, 20)
+
+def test_area(my_rectangle):
+    # result = shapes.rectangle(2, 8)
+    assert my_rectangle.area() == 10 * 20 # Fixtures
     
 
-def test_perimeter():
-    result = shapes.rectangle(3,7)
-    assert result.perimeter() == 2 * (3 + 7)    
+def test_perimeter(my_rectangle):
+    # result = shapes.rectangle(3,7)
+    assert my_rectangle.perimeter() == 2 * (10 + 20)    # Fixtures
+
+# def test_area():
+#     result = shapes.rectangle(2, 8)
+#     assert result.area() == 2 * 8
+    
+
+# def test_perimeter():
+#     result = shapes.rectangle(3,7)
+#     assert result.perimeter() == 2 * (3 + 7)   
